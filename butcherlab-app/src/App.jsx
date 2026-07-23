3d944e0246cf1fb1036b8f81d96534df92165013
cirofazi17 <faziciro.1@gmail.com>
2026-07-23 00:20:37 +0000

    Icona WhatsApp React Icons

--- butcherlab-app/src/App.jsx
+++ butcherlab-app/src/App.jsx
@@ -5,6 +5,7 @@ import SearchBar from './SearchBar'
 import CategoryFilter from './CategoryFilter'
 import AdminPanel from './AdminPanel'
 import { supabase } from './supabase'
+import { FaWhatsapp } from "react-icons/fa";
 
 const impostazioniPredefinite = {
   business_name: 'BUTCHER LAB',
@@ -472,16 +473,7 @@ ${impostazioni.address}
         onClick={apriWhatsAppInformazioni}
         aria-label="Contattaci su WhatsApp"
       >
-        <svg
-  viewBox="0 0 32 32"
-  width="34"
-  height="34"
-  fill="currentColor"
-  aria-hidden="true"
->
-  <path d="M19.11 17.21c-.27-.14-1.6-.79-1.85-.88-.25-.09-.43-.14-.61.14-.18.27-.7.88-.86 1.06-.16.18-.32.2-.59.07-.27-.14-1.14-.42-2.17-1.34-.8-.71-1.34-1.59-1.5-1.86-.16-.27-.02-.42.12-.55.12-.12.27-.32.41-.48.14-.16.18-.27.27-.45.09-.18.05-.34-.02-.48-.07-.14-.61-1.47-.84-2.01-.22-.53-.45-.46-.61-.47h-.52c-.18 0-.48.07-.73.34-.25.27-.95.93-.95 2.27 0 1.34.98 2.63 1.11 2.81.14.18 1.92 2.93 4.65 4.11.65.28 1.16.45 1.56.58.66.21 1.25.18 1.72.11.52-.08 1.6-.66 1.83-1.29.23-.64.23-1.18.16-1.29-.07-.11-.25-.18-.52-.32z"/>
-  <path d="M16.04 3.2c-7.02 0-12.73 5.71-12.73 12.73 0 2.25.59 4.45 1.7 6.38L3.2 28.8l6.65-1.75a12.69 12.69 0 0 0 6.19 1.62h.01c7.02 0 12.73-5.71 12.73-12.73S23.06 3.2 16.04 3.2z"/>
-</svg>
+<FaWhatsapp size={34} />
       </button>
 
       <CartBar
